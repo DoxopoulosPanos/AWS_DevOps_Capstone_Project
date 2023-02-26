83533f413e144e43467b061a76115dda47f1cd53
DoxopoulosPanos <panosdoxos@gmail.com>
2023-02-26 00:56:27 +0000

    remove index.html

--- blue/app.py
+++ blue/app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
-    
-@app.route('/')
-def index():
-    return render_template('index.html')
+    return "Hello, this is the BLUE deployment!"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
